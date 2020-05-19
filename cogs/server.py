04c9a842f98ab9a3f3764b33c94761975c4d50c3
Oli Fog <oliver@fogelin.com>
2020-05-19 14:02:29 +0100

    More debug

--- cogs/server.py
+++ cogs/server.py
@@ -1,4 +1,5 @@
 import asyncio
+import traceback
 
 import discord
 from discord.ext import commands
@@ -314,7 +315,11 @@ class server(commands.Cog):
                         await ctx.send(rolekeys[index])
                         await ctx.send(self.bot)
                         await ctx.send(self.bot.rolecolours)
-                        await ctx.send(self.bot.rolecolours.get(rolekeys[index]))
+                        try:
+                            await ctx.send(self.bot.rolecolours.get(rolekeys[index]))
+                        except Exception as e:
+                            await ctx.send(e)
+                            await ctx.send(traceback.print_exc())
                         colour = self.bot.rolecolours.get(rolekeys[index])
                         await ctx.send(colour)
                         newrole = await ctx.guild.create_role(name=rolekeys[index], colour=colour)
